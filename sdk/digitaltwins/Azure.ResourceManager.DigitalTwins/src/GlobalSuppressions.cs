f15235a3d4a77ae4aee94007456f2c1d47418912
David R. Williamson <drwill@microsoft.com>
2020-06-11 14:57:35 -0700

    feat(adt): track2 control plane SDK

---
+++ sdk/digitaltwins/Azure.ResourceManager.DigitalTwins/src/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Usage",
+    "AZC0001:Use one of the following pre-approved namespace groups (https://azure.github.io/azure-sdk/registered_namespaces.html): Azure.AI, Azure.Analytics, Azure.Data, Azure.DigitalTwins, Azure.Iot, Azure.Media, Azure.Management, Azure.Messaging, Azure.Search, Azure.Security, Azure.Storage, Azure.Template, Azure.Identity, Microsoft.Extensions.Azure",
+    Justification = "<Pending>",
+    Scope = "module")]
+[assembly: SuppressMessage("Usage", "AZC0008:ClientOptions should have a nested enum called ServiceVersion", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.ResourceManager.DigitalTwins.DigitalTwinsManagementClientOptions")]
